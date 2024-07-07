b30f9c11a9620279147aeb59b4eb105fb5d91ed2
Roll <aleksey123336@mail.ru>
2024-07-07 16:46:03 +0300

    ready

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Проектная работа Mesto
-
+# Проектная работа №5: "Mesto".
+## https://github.com/Roll336/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,25 @@
+const placesList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+
+function createCard(name, link, deleteCard) {
+  const card = cardTemplate.querySelector(".card").cloneNode(true);
+  const deleteButton = card.querySelector(".card__delete-button");
+  card.querySelector(".card__title").textContent = name;
+  card.querySelector(".card__image").src = link;
+  card.querySelector(".card__image").alt = "На фотографии " + name;
+  deleteButton.addEventListener("click", deleteCard);
+  return card;
+}
+
+function deleteCard(event) {
+  event.target.closest(".card").remove();
+}
+
+initialCards.forEach((item) => {
+  const newCard = createCard(item.name, item.link, deleteCard);
+  placesList.append(newCard);
+});
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
